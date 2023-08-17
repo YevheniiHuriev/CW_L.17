42907fd7f843264391d6b75ab8275a8a292bfaae
Yevhenii Huriev <g0669995893@gmail.com>
2023-08-17 19:56:57 +0300

    Добавьте файлы проекта.

---
+++ CW_L.17/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+
+
+
+	return 0;
+}

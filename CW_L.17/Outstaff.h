fe633b30e463583197a31f5b748ff6c27556c28b
Yevhenii Huriev <g0669995893@gmail.com>
2023-08-17 20:28:27 +0300

    Add Outstaff

---
+++ CW_L.17/Outstaff.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Employee.h"
+class Outstaff : public Employee
+{
+public: 
+	Outstaff();
+	Outstaff(string name, string position, float salary);
+
+	void show()const override;
+};
+

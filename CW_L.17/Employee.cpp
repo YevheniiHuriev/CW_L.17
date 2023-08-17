2d5be3889ce7431ee3236d2419a3310720d2476d
Yevhenii Huriev <g0669995893@gmail.com>
2023-08-17 20:06:41 +0300

    Add Employee.cpp

--- CW_L.17/Employee.cpp
+++ CW_L.17/Employee.cpp
@@ -2,12 +2,22 @@
 
 Employee::Employee()
 {
+	name = "no";
+	position = "no";
+	salary = 0;
 }
 
 Employee::Employee(string name, string position, float salary)
 {
+	this->name = name;
+	this->position = position;
+	this->salary = salary;
+
 }
 
 void Employee::show() const
 {
+	cout << "Name: " << name << endl;
+	cout << "Position: " << position << endl;
+	cout << "Salary: " << salary << endl;
 }

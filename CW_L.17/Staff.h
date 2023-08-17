1ebe932846a9357736f0d893b22db3ab7a0a03d4
Yevhenii Huriev <g0669995893@gmail.com>
2023-08-17 20:19:52 +0300

    Added description Staff

--- CW_L.17/Staff.h
+++ CW_L.17/Staff.h
@@ -3,5 +3,12 @@
 class Staff :
     public Employee
 {
+protected:
+    float bonus;
+public:
+    Staff();
+    Staff(string name, string position, float salary, float bonus);
+
+    void show()const override;
 };
 

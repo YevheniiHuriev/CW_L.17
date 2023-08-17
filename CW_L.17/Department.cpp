85d7cd876d2699de73b6b3091571708c45731523
Yevhenii Huriev <g0669995893@gmail.com>
2023-08-17 20:38:30 +0300

    Added definition Department

--- CW_L.17/Department.cpp
+++ CW_L.17/Department.cpp
@@ -1,1 +1,14 @@
 #include "Department.h"
+
+void Department::addEmployees(Employee* obj)
+{
+	base.push_back(obj);
+}
+
+void Department::show() const
+{
+	for (int i = 0; i < base.size(); i++)
+	{
+		base[i]->show();
+	}
+}
